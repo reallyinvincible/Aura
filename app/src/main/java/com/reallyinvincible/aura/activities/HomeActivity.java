39e686c763fccf70ed7056064df25fc73734df3e
reallyinvincible <mailtosparshsri@gmail.com>
2019-02-23 05:34:12 +0530

    Final Build

--- app/src/main/java/com/reallyinvincible/aura/activities/HomeActivity.java
+++ app/src/main/java/com/reallyinvincible/aura/activities/HomeActivity.java
@@ -6,11 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.reallyinvincible.aura.AddDisasterBottomFragment;
+import com.reallyinvincible.aura.DialogueControlInterface;
 import com.reallyinvincible.aura.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    AddDisasterBottomFragment addDisasterBottomFragment;
+    private static DialogueControlInterface dialogueControlInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         SharedPreferences sharedPreferences = getSharedPreferences("Information", MODE_PRIVATE);
         String phoneNumber = sharedPreferences.getString("UserPhoneNumber", "0");
+        addDisasterBottomFragment = new AddDisasterBottomFragment();
         ((TextView)findViewById(R.id.tv_user_name)).setText(phoneNumber);
         findViewById(R.id.btn_sample).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +40,33 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(mapDisasterIntent);
             }
         });
+
+        dialogueControlInterface = new DialogueControlInterface() {
+            @Override
+            public void dismiss() {
+                dismissDialogue();
+            }
+        };
+
+        findViewById(R.id.btn_add_data).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialogue();
+            }
+        });
+
     }
+
+    void openDialogue(){
+        addDisasterBottomFragment.show(getSupportFragmentManager(), "AddDisaster");
+    }
+
+    void dismissDialogue(){
+        addDisasterBottomFragment.dismiss();
+    }
+
+    public static DialogueControlInterface getDialogueControlInterface() {
+        return dialogueControlInterface;
+    }
+
 }
--- app/src/main/java/com/reallyinvincible/aura/activities/LoginActivity.java
+++ app/src/main/java/com/reallyinvincible/aura/activities/LoginActivity.java
@@ -8,7 +8,6 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -194,7 +193,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     void launchHome(){
-        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        Intent intent = new Intent(LoginActivity.this, DisasterMapsActivity.class);
         startActivity(intent);
         finishAfterTransition();
     }
